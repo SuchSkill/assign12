1d084f15d115b6f39bcb2cd1efbd01ca158afd29
Shymko Eugene <imjust93@gmail.com>
2017-01-17 22:51:03 +0100

    Initial commit

---
+++ src/main/java/OrderItem.java
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+public class OrderItem {
+	
+	private int id;
+	private Order order;
+	private int count;
+	private Product product;
+	
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+public class Product {
+	
+	private int id;
+	private String name;
+}
